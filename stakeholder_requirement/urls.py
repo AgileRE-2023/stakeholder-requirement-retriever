3d5748c7049d81d98161acb357c62906abfea95c
FauzanRama11 <fauzanzietugas@gmail.com>
2023-10-05 10:18:21 +0700

    About dan FAQ

--- stakeholder_requirement/urls.py
+++ stakeholder_requirement/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from stakeholder_requirement import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('about', views.about),
+    path('faq', views.faq)
 ]
