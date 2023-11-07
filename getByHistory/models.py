f5eef7645a2145f151dcb6f46f557f72b96c9b2c
AlfarizaRasendria <rasendria.alfariza18@gmail.com>
2023-11-07 08:13:35 +0700

    Add Database Schema (History Tabel)

--- getByHistory/models.py
+++ getByHistory/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from getByQuery.models import Prodi
 
 # Create your models here.
+class History(models.Model):
+    date_generated = models.DateTimeField(auto_now_add=True)
+    exp_date = models.DateTimeField(auto_now_add=True)
+    requirements = models.CharField(max_length=200)
+    id_history = models.ForeignKey(Prodi, on_delete=models.CASCADE)
