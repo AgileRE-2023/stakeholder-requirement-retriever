2153d25e70984e04d0ad58911affb5d3dc798a57
AlfarizaRasendria <rasendria.alfariza18@gmail.com>
2023-11-14 04:50:25 +0700

    Update Database Schema : History model

--- getByHistory/models.py
+++ getByHistory/models.py
@@ -4,7 +4,7 @@ from getByQuery.models import Prodi
 # Create your models here.
 class History(models.Model):
     id_history = models.AutoField(primary_key=True)
-    id_prodi = models.ForeignKey(Prodi, on_delete=models.CASCADE)
+    id_prodi = models.ForeignKey(Prodi, on_delete=models.CASCADE, related_name='histories')
     date_generated = models.DateTimeField(auto_now_add=True)
     exp_date = models.DateTimeField(auto_now_add=True)
     requirements = models.CharField(max_length=200)
