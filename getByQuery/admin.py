4a3c4caca57c626af3d6405ddfee0bc9c1aecb4b
FaridFatchur <m.farid.fatchur.rohmad-2021@fst.unair.ac.id>
2023-11-21 01:07:30 +0700

    update models for major

--- getByQuery/admin.py
+++ getByQuery/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Prodi, Scraping
+# Register your models here.
 
+admin.site.register(Prodi)
+admin.site.register(Scraping)
 # Register your models here.
--- getByQuery/models.py
+++ getByQuery/models.py
@@ -3,9 +3,10 @@ from django.db import models
 class Prodi(models.Model):
     id_prodi = models.AutoField(primary_key=True)
     nama_prodi = models.CharField(max_length=50,unique=True,null=False)
+    subject = models.CharField(max_length=1, null=False, default=0) #0 for Arts & Humanities; 1 for Engineering and Technology; 2 for Life Sciences and Medicine; 3 for Natural Sciences
 
 class Scraping(models.Model):
     id_scrap = models.AutoField(primary_key=True)
-    id_prodi = models.ForeignKey(Prodi, on_delete=models.CASCADE, related_name='scrapings')
+    id_prodi = models.ForeignKey(Prodi, on_delete=models.CASCADE, related_name='scrapings', default=1)
     tgl_scrap = models.DateTimeField(auto_now_add=True)
     teks = models.TextField(max_length=599999,null=False)
