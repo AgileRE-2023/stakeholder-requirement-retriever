118e25c16859e3fa1384224f4af138b05f8fddf6
FaridFatchur <m.farid.fatchur.rohmad-2021@fst.unair.ac.id>
2023-11-21 10:04:50 +0700

    Fauzan ask for this

--- getByQuery/views.py
+++ getByQuery/views.py
@@ -4,13 +4,29 @@ from .preprocess import preprocess_data
 from .scrapingKalibrr import scrapingKalibrr
 from .mainprocess import mainProcess
 from .validateInput import validateInput
+from getByQuery.models import Prodi
 # Create your views here.
 def home(request): 
     return render(request, "home.html")
 
 
-def search(request): 
-    return render(request, "search.html")
+def search(request):
+    major = Prodi.objects.all()
+    majorAH = Prodi.objects.filter(subject=0)
+    majorET = Prodi.objects.filter(subject=1)
+    majorLSM = Prodi.objects.filter(subject=2)
+    majorNS = Prodi.objects.filter(subject=3)
+    majorSSM = Prodi.objects.filter(subject=4)
+    major_values = [prodi.nama_prodi for prodi in major]
+    context = {
+        'major':major_values,
+        'majorAH':majorAH,
+        'majorET':majorET,
+        'majorLSM':majorLSM,
+        'majorNS':majorNS,
+        'majorSSM':majorSSM,
+    }
+    return render(request, "search.html",context)
 
 
 def getByQuery(request):
@@ -25,8 +41,6 @@ def getByQuery(request):
         # return HttpResponse(f'your input value: {result}')
     else:
         return HttpResponse("nowhere to go!!!")
-
-from getByQuery.models import Prodi
 
 def majorView(request):
     major = Prodi.objects.all()
