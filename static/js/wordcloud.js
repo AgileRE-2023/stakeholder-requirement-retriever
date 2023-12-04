543654262a82fae7c6abd345899419b8367f8456
achmadaffandifauzan <achmadaffandifauzan@gmail.com>
2023-12-04 21:20:31 +0700

    word cloud fix bug

--- static/js/wordcloud.js
+++ static/js/wordcloud.js
@@ -1,36 +1,60 @@
-let words = [
-    {word: "skill-1", quantity: 95},
-    {word: "skill-2", quantity: 85},
-    {word: "skill-3", quantity: 75},
-    {word: "skill-4", quantity: 65},
-    {word: "skill-5", quantity: 60},
-    {word: "skill-6", quantity: 55},
-    {word: "skill-7", quantity: 45},
-    {word: "skill-8", quantity: 35},
-    {word: "skill-9", quantity: 19},
-    {word: "skill-10", quantity: 10},
-   
-];
+document.addEventListener("DOMContentLoaded", function () {
+  var termsAndScore = document.querySelectorAll(".scoreHidden");
+  var words = [];
 
-let colors = ["#4169E1","#6941e1", "#b9e141","#e141b9", "#41e169", "#dbc970", "#b98e68", "	#af4035", "#88d8c0" ];
+  // Find the minimum and maximum quantity values
+  const minQuantity = Math.min(
+    ...Array.from(termsAndScore, (element) =>
+      parseFloat(element.value.split("_")[1])
+    )
+  );
+  const maxQuantity = Math.max(
+    ...Array.from(termsAndScore, (element) =>
+      parseFloat(element.value.split("_")[1])
+    )
+  );
 
-const width = 500;
-const height = 450;
+  termsAndScore.forEach((element) => {
+    const word = element.value.split("_")[0];
+    const quantity = parseFloat(element.value.split("_")[1]);
 
-const svg = d3
+    // Convert quantity to the 0-100 range using linear scaling
+    const scaledQuantity =
+      ((quantity - minQuantity) / (maxQuantity - minQuantity)) * (100 - 0) + 0;
+
+    // Create an object and push it into the words array
+    words.push({ word: word, quantity: scaledQuantity });
+  });
+
+  console.log(words); // or do something else with the words array
+
+  // Function to generate a random color
+  function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  const width = 500;
+  const height = 450;
+
+  const svg = d3
     .select("section")
     .append("svg")
     .attr("width", width)
     .attr("height", height);
 
-const layout = d3.layout
+  const layout = d3.layout
     .cloud()
     .size([width, height])
     .words(
-        words.map((word) => ({
-            text :word.word,
-            size: word.quantity * 1,
-        }))
+      words.map((word) => ({
+        text: word.word,
+        size: word.quantity * 1,
+      }))
     )
     .padding(2)
     .rotate(0)
@@ -38,25 +62,24 @@ const layout = d3.layout
     .spiral("rectangular")
     .on("end", draw);
 
-layout.start();
+  layout.start();
 
-function draw(words) {
+  function draw(words) {
     svg.selectAll("*").remove();
 
     svg
-    .append("g")
-    .attr("transform", `translate(${width / 2}, ${height / 2})`)
-    .selectAll("text")
-    .data(words)
-    .enter()
-    .append("text")
-    .style("font-size", (d) => d.size + "px")
-    .style("fill", (d, i)=>{
-        return colors[i % colors.length];
-    })
-    .attr("text-anchor", "middle")
-    .attr("transform", (d)=>`translate(${[d.x, d.y]})rotate(${d.rotate})`)
-    .text((d) => d.text);
-}
+      .append("g")
+      .attr("transform", `translate(${width / 2}, ${height / 2})`)
+      .selectAll("text")
+      .data(words)
+      .enter()
+      .append("text")
+      .style("font-size", (d) => d.size + "px")
+      .style("fill", () => getRandomColor())
+      .attr("text-anchor", "middle")
+      .attr("transform", (d) => `translate(${[d.x, d.y]})rotate(${d.rotate})`)
+      .text((d) => d.text);
+  }
 
-console.log(words);
+  console.log(words);
+});
