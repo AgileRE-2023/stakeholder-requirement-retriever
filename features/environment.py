4c29670461ae0d36543f002977633036215739ea
achmadaffandifauzan <achmadaffandifauzan@gmail.com>
2023-12-11 19:54:49 +0700

    BDD test for User Story Get_Stakeholder_Requirements_From_Database_In_Search_Page with 3 scenario

---
+++ features/environment.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+
+def before_scenario(context, scenario):
+    context.browser = webdriver.Chrome()  
+
+def after_scenario(context, scenario):
+    context.browser.quit()
