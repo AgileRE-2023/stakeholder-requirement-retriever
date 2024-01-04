517881e82f53a10904bf3cc8f513e17c8e63209e
achmadaffandifauzan <achmadaffandifauzan@gmail.com>
2024-01-04 16:52:22 +0700

    revision on bdd 'see new version' feature

--- features/steps/latest_version_steps.py
+++ features/steps/latest_version_steps.py
@@ -1,6 +1,9 @@
-from behave import when, then
+from behave import *
 from selenium.webdriver.common.by import By
 import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from dateutil import parser
 
 @given('the user is on the major list page')
 def step_given(context):
@@ -12,16 +15,43 @@ def step_when_click_major(context, major):
     major_button = context.browser.find_element(By.XPATH, f'//li[contains(text(), "{major}")]')
     major_button.click()
 
+@when('the user sees the last updated date')
+def step_when_sees_date(context):
+    last_updated_text = context.browser.find_element(By.CLASS_NAME, "last_up").text
+    # Parse the string to a datetime object using dateutil.parser.parse
+    last_updated_datetime = parser.parse(last_updated_text, fuzzy=True)
+    # Convert the datetime object to a timestamp
+    timestamp = time.mktime(last_updated_datetime.timetuple())
+    # Store the timestamp in the context for later use
+    context.last_updated_timestamp = timestamp
+
 @when('the user clicks on the "see new version" button')
 def step_when_click_latest_version(context):
-    latest_version_button = context.browser.find_element(By.ID, "new_version_button")
-    latest_version_button.click()
-    time.sleep(2)
+    # Get the initial timestamp before submitting the form
+    initial_timestamp = get_current_timestamp(context)
+
+    # Find the form element and submit it
+    search_form = context.browser.find_element(By.ID, "search-form")
+    search_form.submit()
+
+    # Wait for the timestamp to change
+    WebDriverWait(context.browser, timeout=240).until(
+        lambda browser: get_current_timestamp(context) > initial_timestamp
+    )
+
+def get_current_timestamp(context):
+    # Extract and parse the timestamp from the page
+    last_updated_text = context.browser.find_element(By.CLASS_NAME, "last_up").text
+    last_updated_datetime = parser.parse(last_updated_text, fuzzy=True)
+    return time.mktime(last_updated_datetime.timetuple())
 
 @then('the user should see the latest stakeholder requirements in the output page')
 def step_then_latest_requirements(context):
-    last_updated_element = context.browser.find_element(By.CLASS_NAME, "last_up")
-    last_updated_text = last_updated_element.text.split("Last Updated ")[1]
+    last_updated_text = context.browser.find_element(By.CLASS_NAME, "last_up").text
 
-    latest_requirements_info = context.browser.find_element(By.ID, "output_req").text
-    assert last_updated_text in latest_requirements_info, "Latest requirements not found on the page"
+    # Parse to a timestamp using dateutil.parser.parse
+    last_updated_datetime = parser.parse(last_updated_text, fuzzy=True)
+    new_timestamp = time.mktime(last_updated_datetime.timetuple())
+
+    # Compare the new timestamp with the stored timestamp
+    assert new_timestamp > context.last_updated_timestamp, "New timestamp is not newer than the stored timestamp, the requirements are not updated!"
