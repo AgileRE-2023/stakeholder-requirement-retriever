ca587bbb9967846feeeb381c277e3e7fe7aff2d6
CokiNababan <150018412+CokiNbbn@users.noreply.github.com>
2024-01-05 22:47:39 +0700

    update BDD for new_version feature to follow behat rules

--- features/latest_version_steps.feature
+++ features/latest_version_steps.feature
@@ -1,8 +1,14 @@
-Feature: Get latest requirements from button on the output page
+Feature: User Story Get_Latest_Requirements_From_Button_On_Output_Page
+    As a user of the stakeholder requirement retrieval app
+    I want to view the latest stakeholder requirements for a major
+    So that I can stay updated on the current requirements
+
+  Feature: Get latest requirements from button on the output page
 
     Scenario: User clicks on "see new version" button to get the latest requirements
-        Given the user is on the major list page
-        When the user clicks "Electrical Engineering"
-        And the user sees the last updated date
-        And the user clicks on the "see new version" button
-        Then the user should see the latest stakeholder requirements in the output page by knowing that current date of requirement terms is newer than the previous one
+        Given I am on "/history/major"
+        When I press "Electrical Engineering" 
+        And I see "last updated date"
+        And I press "see new version"
+        Then I should see "the latest stakeholder requirements" in the output page
+        And the current date of requirement terms is newer than the previous one
--- features/steps/latest_version_steps.py
+++ features/steps/latest_version_steps.py
@@ -5,27 +5,22 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from dateutil import parser
 
-@given('the user is on the major list page')
+@given('I am on "/history/major" page')
 def step_given(context):
     context.browser.get('http://127.0.0.1:8000/history/major') 
 
-@when('the user clicks "{major}"')
+@when('I press "{major}"')
 def step_when_click_major(context, major):
-    print(major)
     major_button = context.browser.find_element(By.XPATH, f'//li[contains(text(), "{major}")]')
     major_button.click()
 
-@when('the user sees the last updated date')
+@when('I see "last updated date"')
 def step_when_sees_date(context):
     last_updated_text = context.browser.find_element(By.CLASS_NAME, "last_up").text
-    # Parse the string to a datetime object using dateutil.parser.parse
     last_updated_datetime = parser.parse(last_updated_text, fuzzy=True)
-    # Convert the datetime object to a timestamp
-    timestamp = time.mktime(last_updated_datetime.timetuple())
-    # Store the timestamp in the context for later use
-    context.last_updated_timestamp = timestamp
+    context.last_updated_timestamp = time.mktime(last_updated_datetime.timetuple())
 
-@when('the user clicks on the "see new version" button')
+@when('I press "see new version"')
 def step_when_click_latest_version(context):
     # Get the initial timestamp before submitting the form
     initial_timestamp = get_current_timestamp(context)
@@ -40,18 +35,15 @@ def step_when_click_latest_version(context):
     )
 
 def get_current_timestamp(context):
-    # Extract and parse the timestamp from the page
     last_updated_text = context.browser.find_element(By.CLASS_NAME, "last_up").text
     last_updated_datetime = parser.parse(last_updated_text, fuzzy=True)
     return time.mktime(last_updated_datetime.timetuple())
 
-@then('the user should see the latest stakeholder requirements in the output page by knowing that current date of requirement terms is newer than the previous one')
+@then('I should see "the latest stakeholder requirements" in the output page')
 def step_then_latest_requirements(context):
-    last_updated_text = context.browser.find_element(By.CLASS_NAME, "last_up").text
+    assert "the latest stakeholder requirements" in context.browser.page_source, "Latest requirements not found on the page"
 
-    # Parse to a timestamp using dateutil.parser.parse
-    last_updated_datetime = parser.parse(last_updated_text, fuzzy=True)
-    new_timestamp = time.mktime(last_updated_datetime.timetuple())
-
-    # Compare the new timestamp with the stored timestamp
-    assert new_timestamp > context.last_updated_timestamp, "New timestamp is not newer than the stored timestamp, the requirements are not updated!"
+@then('the current date of requirement terms is newer than the previous one')
+def step_then_newer_date(context):
+    new_timestamp = get_current_timestamp(context)
+    assert new_timestamp > context.last_updated_timestamp, "New timestamp is not newer than the stored timestamp, requirements are not updated!"
